c12fd7ec3500cec7d822e1695b06ffe4c2fc2594
graugnat1 <tom.graugnard@gmail.com>
2019-03-06 16:48:02 +0100

    c mieux quavant

--- TPBLAS/src/gemm.c
+++ TPBLAS/src/gemm.c
@@ -1,26 +1,26 @@
 #include "mnblas.h"
 #include "complexe.h"
 
-void cblas_sgemm(const CBLAS_LAYOUT Layout, const CBLAS_TRANSPOSE transa,
-				 const CBLAS_TRANSPOSE transb, const MKL_INT m, const MKL_INT n,
-				 const MKL_INT k, const float alpha, const float *a, const MKL_INT lda,
-				 const float *b, const MKL_INT ldb, const float beta, float *c,
-				 const MKL_INT ldc);
+void mncblas_sgemm(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
+				   MNCBLAS_TRANSPOSE TransB, const int M, const int N,
+				   const int K, const float alpha, const float *A,
+				   const int lda, const float *B, const int ldb,
+				   const float beta, float *C, const int ldc);
 
-void cblas_dgemm(const CBLAS_LAYOUT Layout, const CBLAS_TRANSPOSE transa,
-				 const CBLAS_TRANSPOSE transb, const MKL_INT m, const MKL_INT n,
-				 const MKL_INT k, const double alpha, const double *a, const MKL_INT lda,
-				 const double *b, const MKL_INT ldb, const double beta, double *c,
-				 const MKL_INT ldc);
+void mncblas_dgemm(MNCBLAS_LAYOUT layout, CBLAS_TRANSPOSE TransA,
+				   MNCBLAS_TRANSPOSE TransB, const int M, const int N,
+				   const int K, const double alpha, const double *A,
+				   const int lda, const double *B, const int ldb,
+				   const double beta, double *C, const int ldc);
 
-void cblas_cgemm(const CBLAS_LAYOUT Layout, const CBLAS_TRANSPOSE transa,
-				 const CBLAS_TRANSPOSE transb, const MKL_INT m, const MKL_INT n,
-				 const MKL_INT k, const void *alpha, const void *a, const MKL_INT lda,
-				 const void *b, const MKL_INT ldb, const void *beta, void *c,
-				 const MKL_INT ldc);
+void mncblas_cgemm(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
+				   MNCBLAS_TRANSPOSE TransB, const int M, const int N,
+				   const int K, const void *alpha, const void *A,
+				   const int lda, const void *B, const int ldb,
+				   const void *beta, void *C, const int ldc);
 
-void cblas_zgemm(const CBLAS_LAYOUT Layout, const CBLAS_TRANSPOSE transa,
-				 const CBLAS_TRANSPOSE transb, const MKL_INT m, const MKL_INT n,
-				 const MKL_INT k, const void *alpha, const void *a, const MKL_INT lda,
-				 const void *b, const MKL_INT ldb, const void *beta, void *c,
-				 const MKL_INT ldc);
+void mncblas_zgemm(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
+				   MNCBLAS_TRANSPOSE TransB, const int M, const int N,
+				   const int K, const void *alpha, const void *A,
+				   const int lda, const void *B, const int ldb,
+				   const void *beta, void *C, const int ldc);
