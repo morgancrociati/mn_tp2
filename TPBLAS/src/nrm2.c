a4d32f64df8c35eb8608c82eba8e8d67dae7227a
graugnat1 <tom.graugnard@gmail.com>
2019-03-06 16:37:58 +0100

    Template

--- TPBLAS/src/nrm2.c
+++ TPBLAS/src/nrm2.c
@@ -29,6 +29,32 @@ double mnblas_dnrm2(const int N, const double *X, const int incX)
 	return sqrt(result);
 }
 
-float mnblas_scnrm2(const int N, const void *X, const int incX);
+float mnblas_scnrm2(const int N, const void *X, const int incX){
+	register unsigned int i = 0;
 
-double mnblas_dznrm2(const int N, const void *X, const int incX);
+	register float result;
+
+	register complexe_float_t *_X = X;
+
+	for (; (i < N); i += incX)
+	{
+		result = result + mult_complexe_float(_X,_X);
+	}
+
+	return sqrt(result);
+}
+
+double mnblas_dznrm2(const int N, const void *X, const int incX){
+	register unsigned int i = 0;
+
+	register double result;
+
+	register complexe_double_t *_X = X;
+
+	for (; (i < N); i += incX)
+	{
+		result = result + mult_complexe_float(_X,_X);
+	}
+
+	return sqrt(result);
+}
