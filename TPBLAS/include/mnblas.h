644404ec4d114f722f0dc85c4e60c72a4b3e6f9f
boyquang96 <nhatquang.ho.96@gmail.com>
2019-03-20 16:56:03 +0100

    erreur corrigé

--- TPBLAS/include/mnblas.h
+++ TPBLAS/include/mnblas.h
@@ -204,7 +204,7 @@ void mncblas_sgemm(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
                  const int lda, const float *B, const int ldb,
                  const float beta, float *C, const int ldc);
 
-void mncblas_dgemm(MNCBLAS_LAYOUT layout, CBLAS_TRANSPOSE TransA,
+void mncblas_dgemm(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
                  MNCBLAS_TRANSPOSE TransB, const int M, const int N,
                  const int K, const double alpha, const double *A,
                  const int lda, const double *B, const int ldb,
