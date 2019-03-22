5d24cbd9925a677a45403fbf8a5318b39f0de8da
graugnat1 <tom.graugnard@gmail.com>
2019-03-22 20:56:05 +0100

    perf_axpy + change copy

--- TPBLAS/examples/perf_axpy.c
+++ TPBLAS/examples/perf_axpy.c
@@ -65,13 +65,13 @@ int main(int argc, char **argv)
         float *vec2 = new_s_full(VECSIZE);
 
         start = _rdtsc();
-        mncblas_scopy(VECSIZE, vec1, 1, vec2, 1);
+        mncblas_saxpy(VECSIZE,5.0, vec1, 1, vec2, 1);
         end = _rdtsc();
 
-        printf("mncblas_scopy %d : ", i);
+        printf("mncblas_saxpy %d : ", i);
         vector_print_f(vec2);
         printf("nombre de cycles: %Ld \n", end - start);
-        calcul_flop("scopy ", 2 * VECSIZE, end - start);
+        calcul_flop("saxpy ", 2 * VECSIZE, end - start);
     }
     for (i = 0; i < NB_FOIS; i++)
     {
@@ -79,40 +79,46 @@ int main(int argc, char **argv)
         double *vec4 = new_d_full(VECSIZE);
 
         start = _rdtsc();
-        mncblas_dcopy(VECSIZE, vec3, 1, vec4, 1);
+        mncblas_daxpy(VECSIZE,5.0, vec3, 1, vec4, 1);
         end = _rdtsc();
 
-        printf("mncblas_dcopy %d : ", i);
+        printf("mncblas_daxpy %d : ", i);
         vector_print_f(vec4);
         printf("nombre de cycles: %Ld \n", end - start);
-        calcul_flop("dcopy ", 2 * VECSIZE, end - start);
+        calcul_flop("daxpy ", 2 * VECSIZE, end - start);
     }
     for (i = 0; i < NB_FOIS; i++)
     {
         complexe_float_t *vec5 = new_c_full(VECSIZE);
         complexe_float_t *vec6 = new_c_full(VECSIZE);
+        complexe_float_t a;
+        a.real = 2.0;
+        a.imaginary = 2.0;
 
         start = _rdtsc();
-        mncblas_ccopy(VECSIZE, vec5, 1, vec6, 1);
+        mncblas_caxpy(VECSIZE,a, vec5, 1, vec6, 1);
         end = _rdtsc();
 
-        printf("mncblas_ccopy %d : ", i);
+        printf("mncblas_caxpy %d : ", i);
         vector_print_f(vec6);
         printf("nombre de cycles: %Ld \n", end - start);
-        calcul_flop("ccopy ", 2 * VECSIZE, end - start);
+        calcul_flop("caxpy ", 2 * VECSIZE, end - start);
     }
     for (i = 0; i < NB_FOIS; i++)
     {
         complexe_double_t *vec7 = new_z_full(VECSIZE);
         complexe_double_t *vec8 = new_z_full(VECSIZE);
+        complexe_double_t a;
+        a.real = 2.0;
+        a.imaginary = 2.0;
 
         start = _rdtsc();
-        mncblas_zcopy(VECSIZE, vec7, 1, vec8, 1);
+        mncblas_zaxpy(VECSIZE,a, vec7, 1, vec8, 1);
         end = _rdtsc();
 
-        printf("mncblas_zcopy %d : ", i);
+        printf("mncblas_zaxpy %d : ", i);
         vector_print_f(vec8);
         printf("nombre de cycles: %Ld \n", end - start);
-        calcul_flop("zcopy ", 2 * VECSIZE, end - start);
+        calcul_flop("zaxpy ", 2 * VECSIZE, end - start);
     }
 }
