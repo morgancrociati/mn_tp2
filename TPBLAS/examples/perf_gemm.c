7064b99a457b4320af895def4c8117cb3d3925a1
Morgan Crociati <morgan.crociati@outlook.com>
2019-03-22 23:47:45 +0100

    finished

--- TPBLAS/examples/perf_gemm.c
+++ TPBLAS/examples/perf_gemm.c
@@ -3,160 +3,57 @@
 
 #include "mnblas.h"
 #include "complexe.h"
+#include "blas_test.h"
 
-#include "flop.h"
+#define VECSIZE 128
+#define M 128
+#define N 128
+#define K 128
 
-#define VECSIZE 1024
-#define M 32
-#define N 32
-#define K 32
-
-#define NB_FOIS 5
-
-void vector_print_f(float *V)
-{
-    register unsigned int i;
-    register unsigned int j;
-
-    for (i = 0; i < M; i++){
-        for(j = 0; j < N; j < 0){
-            printf("%f ", V[i]);
-        }
-        printf("\n");
-    }
-
-    printf("\n");
-
-    return;
-}
-
-void vector_print_d(double *V)
-{
-    register unsigned int i;
-    register unsigned int j;
-
-    for (i = 0; i < M; i++){
-        for(j = 0; j < N; j < 0){
-            printf("%f ", V[i]);
-        }
-        printf("\n");
-    }
-
-    printf("\n");
-
-    return;
-}
-
-void vector_print_c(complexe_float_t *V)
-{
-    register unsigned int i;
-    register unsigned int j;
-
-    for (i = 0; i < M; i++){
-        for(j = 0; j < N; j < 0){
-            printf("(%f + i%f) ", V[i].real, V[i].imaginary);
-        }
-        printf("\n");
-    }
-
-    printf("\n");
-
-    return;
-}
-
-void vector_print_z(complexe_double_t *V)
-{
-    register unsigned int i;
-    register unsigned int j;
-
-    for (i = 0; i < M; i++){
-        for(j = 0; j < N; j < 0){
-            printf("(%f + i%f) ", V[i].real, V[i].imaginary);
-        }
-        printf("\n");
-    }
-
-    return;
-}
+#define NB_FOIS 512
 
 int main(int argc, char **argv)
 {
-    unsigned long long start, end;
-    int i;
+	unsigned long long start, end;
+	int i;
 
-    for (i = 0; i < NB_FOIS; i++)
-    {
-        float *C = new_s_full(M * N);
-        float *B = new_s_full(M * K);
-        float *A = new_s_full(K * N);
+	float *C1 = new_s_full(M * N);
+	float *B1 = new_s_full(M * K);
+	float *A1 = new_s_full(K * N);
+	start = _rdtsc();
+	for (i = 0; i < NB_FOIS; i++)
+		mncblas_sgemm(MNCblasRowMajor, MNCblasNoTrans, MNCblasNoTrans, M, N, K, 5.0, A1, 1, B1, 1, 5.0, C1, 1);
+	end = _rdtsc();
+	calcul_flop("sgemm ", 4 * VECSIZE, end - start);
 
-        start = _rdtsc();
-        mncblas_sgemm(MNCblasRowMajor, MNCblasNoTrans, MNCblasNoTrans, M, N, K, 5.0, A, 1, B, 1, 5.0, C, 1);
-        end = _rdtsc();
+	double *C2 = new_d_full(M * N);
+	double *B2 = new_d_full(M * K);
+	double *A2 = new_d_full(K * N);
+	start = _rdtsc();
+	for (i = 0; i < NB_FOIS; i++)
+		mncblas_dgemm(MNCblasRowMajor, MNCblasNoTrans, MNCblasNoTrans, M, N, K, 5.0, A2, 1, B2, 1, 5.0, C2, 1);
+	end = _rdtsc();
+	calcul_flop("dgemm ", 4 * VECSIZE, end - start);
 
-        printf("mncblas_sgemm %d : ", i);
-        vector_print_f(C);
-        printf("nombre de cycles: %Ld \n", end - start);
-        calcul_flop("sgemm ", 4 * VECSIZE, end - start);
-    }
+	complexe_float_t *C3 = new_c_full(M * N);
+	complexe_float_t *B3 = new_c_full(M * K);
+	complexe_float_t *A3 = new_c_full(K * N);
+	complexe_float_t a1 = {2.0, 2.0};
+	complexe_float_t b1 = {2.0, 2.0};
+	start = _rdtsc();
+	for (i = 0; i < NB_FOIS; i++)
+		mncblas_cgemm(MNCblasRowMajor, MNCblasNoTrans, MNCblasNoTrans, M, N, K, &a1, A3, 1, B3, 1, &b1, C3, 1);
+	end = _rdtsc();
+	calcul_flop("cgemm ", 22 * VECSIZE, end - start);
 
-    for (i = 0; i < NB_FOIS; i++)
-    {
-        double *C = new_d_full(M * N);
-        double *B = new_d_full(M * K);
-        double *A = new_d_full(K * N);
-
-        start = _rdtsc();
-        mncblas_dgemm(MNCblasRowMajor, MNCblasNoTrans, MNCblasNoTrans, M, N, K, 5.0, A, 1, B, 1, 5.0, C, 1);
-        end = _rdtsc();
-
-        printf("mncblas_dgemm %d : ", i);
-        vector_print_f(C);
-        printf("nombre de cycles: %Ld \n", end - start);
-        calcul_flop("dgemm ", 4 * VECSIZE, end - start);
-    }
-
-    for (i = 0; i < NB_FOIS; i++)
-    {
-        complexe_float_t *C = new_c_full(M * N);
-        complexe_float_t *B = new_c_full(M * K);
-        complexe_float_t *A = new_c_full(K * N);
-
-        complexe_float_t *a, *b;
-        a->imaginary = 2.0;
-        a->real = 2.0;
-        b->imaginary = 2.0;
-        b->real = 2.0;
-
-        start = _rdtsc();
-        mncblas_cgemm(MNCblasRowMajor, MNCblasNoTrans, MNCblasNoTrans, M, N, K, a, A, 1, B, 1, b, C, 1);
-        end = _rdtsc();
-
-        printf("mncblas_cgemm %d : ", i);
-        vector_print_f(C);
-        printf("nombre de cycles: %Ld \n", end - start);
-        calcul_flop("cgemm ", 22 * VECSIZE, end - start);
-    }
-
-    for (i = 0; i < NB_FOIS; i++)
-    {
-        complexe_double_t *C = new_z_full(M * N);
-        complexe_double_t *B = new_z_full(M * K);
-        complexe_double_t *A = new_z_full(K * N);
-
-        complexe_double_t *a, *b;
-        a->imaginary = 2.0;
-        a->real = 2.0;
-        b->imaginary = 2.0;
-        b->real = 2.0;
-
-        start = _rdtsc();
-        mncblas_zgemm(MNCblasRowMajor, MNCblasNoTrans, MNCblasNoTrans, M, N, K, a, A, 1, B, 1, b, C, 1);
-        end = _rdtsc();
-
-        printf("mncblas_zgemm %d : ", i);
-        vector_print_f(C);
-        printf("nombre de cycles: %Ld \n", end - start);
-        calcul_flop("zgemm ", 22 * VECSIZE, end - start);
-    }
+	complexe_double_t *C4 = new_z_full(M * N);
+	complexe_double_t *B4 = new_z_full(M * K);
+	complexe_double_t *A4 = new_z_full(K * N);
+	complexe_double_t a2 = {2.0, 2.0};
+	complexe_double_t b2 = {2.0, 2.0};
+	start = _rdtsc();
+	for (i = 0; i < NB_FOIS; i++)
+		mncblas_zgemm(MNCblasRowMajor, MNCblasNoTrans, MNCblasNoTrans, M, N, K, &a2, A4, 1, B4, 1, &b2, C4, 1);
+	end = _rdtsc();
+	calcul_flop("zgemm ", 22 * VECSIZE, end - start);
 }
--- TPBLAS/examples/perf_gemv.c
+++ TPBLAS/examples/perf_gemv.c
@@ -3,137 +3,56 @@
 
 #include "mnblas.h"
 #include "complexe.h"
+#include "blas_test.h"
 
-#include "flop.h"
+#define VECSIZE 2048
+#define MATSIZE 1024
 
-#define VECSIZE 1024
-#define M 32
-#define N 32
-
-#define NB_FOIS 5
-
-void vector_print_f(float *V)
-{
-    register unsigned int i;
-
-    for (i = 0; i < VECSIZE; i++)
-        printf("%f ", V[i]);
-    printf("\n");
-
-    return;
-}
-
-void vector_print_d(double *V)
-{
-    register unsigned int i;
-
-    for (i = 0; i < VECSIZE; i++)
-        printf("%f ", V[i]);
-    printf("\n");
-
-    return;
-}
-
-void vector_print_c(complexe_float_t *V)
-{
-    register unsigned int i;
-
-    for (i = 0; i < VECSIZE; i++)
-        printf("(%f + i%f) ", V[i].real, V[i].imaginary);
-    printf("\n");
-
-    return;
-}
-
-void vector_print_z(complexe_double_t *V)
-{
-    register unsigned int i;
-
-    for (i = 0; i < VECSIZE; i++)
-        printf("(%f + i%f) ", V[i].real, V[i].imaginary);
-    printf("\n");
-
-    return;
-}
+#define NB_FOIS 512
 
 int main(int argc, char **argv)
 {
-    unsigned long long start, end;
-    int i;
+	register unsigned long long start;
+	register unsigned long long end;
+	register unsigned long i;
 
-    for (i = 0; i < NB_FOIS; i++)
-    {
-        float *vec1 = new_s_full(VECSIZE);
-        float *vec2 = new_s_full(VECSIZE);
-        float *A = new_s_full(M * N);
+	float *vec1 = new_s_full(VECSIZE);
+	float *vec2 = new_s_full(VECSIZE);
+	float *A1 = new_s_full(VECSIZE * MATSIZE);
+	start = _rdtsc();
+	for (i = 0; i < NB_FOIS; i++)
+		mncblas_sgemv(MNCblasRowMajor, MNCblasNoTrans, VECSIZE, MATSIZE, 5.0, A1, 1, vec1, 1, 5.0, vec2, 1);
+	end = _rdtsc();
+	calcul_flop("sgemv ", 4 * VECSIZE, end - start);
 
-        start = _rdtsc();
-        mncblas_sgemv(MNCblasRowMajor, MNCblasNoTrans, M, N, 5.0, A, 1, vec1, 1, 5.0, vec2, 1);
-        end = _rdtsc();
+	double *vec3 = new_d_full(VECSIZE);
+	double *vec4 = new_d_full(VECSIZE);
+	double *A2 = new_d_full(VECSIZE * MATSIZE);
+	start = _rdtsc();
+	for (i = 0; i < NB_FOIS; i++)
+		mncblas_dgemv(MNCblasRowMajor, MNCblasNoTrans, VECSIZE, MATSIZE, 5.0, A2, 1, vec3, 1, 5.0, vec4, 1);
+	end = _rdtsc();
+	calcul_flop("dgemv ", 4 * VECSIZE, end - start);
 
-        printf("mncblas_sgemv %d : ", i);
-        vector_print_f(vec2);
-        printf("nombre de cycles: %Ld \n", end - start);
-        calcul_flop("sgemv ", 4 * VECSIZE, end - start);
-    }
+	complexe_float_t *vec5 = new_c_full(VECSIZE);
+	complexe_float_t *vec6 = new_c_full(VECSIZE);
+	complexe_float_t *A3 = new_c_full(VECSIZE * MATSIZE);
+	complexe_float_t a1 = {2.0, 2.0};
+	complexe_float_t b1 = {2.0, 2.0};
+	start = _rdtsc();
+	for (i = 0; i < NB_FOIS; i++)
+		mncblas_cgemv(MNCblasRowMajor, MNCblasNoTrans, VECSIZE, MATSIZE, &a1, A3, 1, vec5, 1, &b1, vec6, 1);
+	end = _rdtsc();
+	calcul_flop("cgemv ", 22 * VECSIZE, end - start);
 
-    for (i = 0; i < NB_FOIS; i++)
-    {
-        double *vec3 = new_d_full(VECSIZE);
-        double *vec4 = new_d_full(VECSIZE);
-        double *A = new_d_full(M * N);
-
-        start = _rdtsc();
-        mncblas_dgemv(MNCblasRowMajor, MNCblasNoTrans, M, N, 5.0, A, 1, vec3, 1, 5.0, vec4, 1);
-        end = _rdtsc();
-
-        printf("mncblas_dgemv %d : ", i);
-        vector_print_f(vec4);
-        printf("nombre de cycles: %Ld \n", end - start);
-        calcul_flop("dgemv ", 4 * VECSIZE, end - start);
-    }
-
-    for (i = 0; i < NB_FOIS; i++)
-    {
-        complexe_float_t *vec5 = new_c_full(VECSIZE);
-        complexe_float_t *vec6 = new_c_full(VECSIZE);
-        complexe_float_t *A = new_c_full(M * N);
-
-        complexe_float_t *a, *b;
-        a->imaginary = 2.0;
-        a->real = 2.0;
-        b->imaginary = 2.0;
-        b->real = 2.0;
-
-        start = _rdtsc();
-        mncblas_cgemv(MNCblasRowMajor, MNCblasNoTrans, M, N, a, A, 1, vec5, 1, b, vec6, 1);
-        end = _rdtsc();
-
-        printf("mncblas_cgemv %d : ", i);
-        vector_print_f(vec6);
-        printf("nombre de cycles: %Ld \n", end - start);
-        calcul_flop("cgemv ", 22 * VECSIZE, end - start);
-    }
-
-    for (i = 0; i < NB_FOIS; i++)
-    {
-        complexe_double_t *vec7 = new_z_full(VECSIZE);
-        complexe_double_t *vec8 = new_z_full(VECSIZE);
-        complexe_double_t *A = new_z_full(M * N);
-
-        complexe_double_t *a, *b;
-        a->imaginary = 2.0;
-        a->real = 2.0;
-        b->imaginary = 2.0;
-        b->real = 2.0;
-
-        start = _rdtsc();
-        mncblas_zgemv(MNCblasRowMajor, MNCblasNoTrans, M, N, a, A, 1, vec7, 1, b, vec8, 1);
-        end = _rdtsc();
-
-        printf("mncblas_zgemv %d : ", i);
-        vector_print_f(vec8);
-        printf("nombre de cycles: %Ld \n", end - start);
-        calcul_flop("zgemv ", 22 * VECSIZE, end - start);
-    }
+	complexe_double_t *vec7 = new_z_full(VECSIZE);
+	complexe_double_t *vec8 = new_z_full(VECSIZE);
+	complexe_double_t *A4 = new_z_full(VECSIZE * MATSIZE);
+	complexe_double_t a2 = {2.0, 2.0};
+	complexe_double_t b2 = {2.0, 2.0};
+	start = _rdtsc();
+	for (i = 0; i < NB_FOIS; i++)
+		mncblas_zgemv(MNCblasRowMajor, MNCblasNoTrans, VECSIZE, MATSIZE, &a2, A4, 1, vec7, 1, &b2, vec8, 1);
+	end = _rdtsc();
+	calcul_flop("zgemv ", 22 * VECSIZE, end - start);
 }
